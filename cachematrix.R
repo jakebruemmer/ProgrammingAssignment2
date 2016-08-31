4c008b58abae46eebf2e44c6fde7fab4f96cb127
gr1zzly-be4r <jake.bruemmer@gmail.com>
2016-08-30 22:09:44 -0500

    Committing hw assingment.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
+# I wrote this function to do what it's supposed to do
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x # This just returns the object
+  solveinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(get = get, solveinverse = solveinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+# comment
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("Already calculated")
+    return(i)
+  }
+  d <- x$get()
+  i <- solve(d, ...)
+  x$solveinverse(i)
+  i
 }
